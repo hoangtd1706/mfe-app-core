7d9375e56779edf2977812e0446ccbca32c5cdce
hoangtd1706 <hoangtd1706@gmail.com>
2024-11-19 12:01:23 +0700

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { AppContextProvider, useAppContext } from "./contexts/app.context";
+export { default as axiosBuilder } from "./axiosBuilder";
+export { default as Layout } from "./layouts/Layout";
+export { default as AppBar } from "./components/AppBar";
+export { default as PrintLog } from "./utils/printLog";
+export { default as RemoteVNMTones } from "./utils/removeVietnameseTones";
+export { default as useWindowSize } from "./utils/useWindowSize";
 export * from "./types";
--- src/utils/printLog.ts
+++ src/utils/printLog.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export function PrintLog(message: string, obj?: any) {
+export default function PrintLog(message: string, obj?: any) {
   console.log(`[${moment().format("ddd, DD MMM YYYY HH:mm:ss")}] : ${message}`);
   if (obj !== undefined) console.log(obj);
 }
